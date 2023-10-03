9df241d322f0f2cc4b5416db947bb830f9de42ea
DimaRyndya <dmitriy.ryndya@gmail.com>
2023-10-03 19:52:19 +0300

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Dmitriy Ryndya on 03.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
