3fb65d8ecfb6bce250aeebb85fff111307ae56a5
DimaRyndya <dmitriy.ryndya@gmail.com>
2024-01-18 19:55:00 +0200

    Rewrite UIImageView extension

--- Sources/Common/Extensions/UIImageView.swift
+++ Sources/Common/Extensions/UIImageView.swift
@@ -3,16 +3,14 @@ import Foundation
 import UIKit
 
 extension UIImageView {
-    
     func loadImage(url: String) {
         guard let imageURL = URL(string: url) else { return }
         DispatchQueue.global().async { [weak self] in
-            guard let data = try? Data(contentsOf: imageURL) else { return }
-            if let image = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self?.image = image
-                }
+            guard let data = try? Data(contentsOf: imageURL), let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                self?.image = image
             }
+
         }
     }
 }
